87ccf19702b47035b2b2c5cce2ac9a89d9bff5d6
David <soaresdavid411@gmail.com>
2024-02-08 17:52:37 -0300

    feat: creted service for add product.

--- src/Services/GetCategories.jsx
+++ src/Services/GetCategories.jsx
@@ -1,16 +1,31 @@
 import axios from "axios";
 
-const GetProductCategories = async () =>  {
-    try {
-        const categories = await axios.get(`http://localhost:3000/api/produtos/GetCategoryProduct`, {
-            headers: {
-                Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAZ21haWwuY29tIiwicGFzc3dvcmQiOiIkMmIkMTAkNDIwSXBuU2xHRXNVWHVrRHJ6b3BWT3F5ejF0dkhuNURBSWZiVi9HSjFHMi45L1dsRm5sOTYiLCJpYXQiOjE3MDczMTYwODAsImV4cCI6MTcwNzMxOTY4MH0.CsVZv23LH6wENROipfS4Ugk9ROi1L8GWnOmxgj98aT0`, // Assuming your token follows the Bearer schema
-              },
-        });
-        return categories.data;
-    } catch (error) {
-        return console.error("Erro ao obter as categorias:", error);        
-    }
-}
+const GetProductCategories = async () => {
+  try {
+    const categories = await axios.get(
+      `http://localhost:3000/api/produtos/GetCategoryProduct`,
+      {
+        headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAZ21haWwuY29tIiwicGFzc3dvcmQiOiIkMmIkMTAkdEFWR1RQSi8vb0VSWks5YlVPWXhrZXo4UnUvaWlwLlRlZmJtOVhZWDZMam5TWHF2VDZ5ZksiLCJpYXQiOjE3MDc0MjMxMzUsImV4cCI6MTcwNzQyNjczNX0.zZxyPru6XtTp5lIk5udVLFh-huY3hvXMJB_a8qnLTOA`,        },
+      }
+    );
+    return categories.data;
+  } catch (error) {
+    return console.error("Erro ao obter as categorias:", error);
+  }
+};
 
-export default GetProductCategories;
+const CreateProduct = async (data) => {
+  try {
+    await axios.post("http://localhost:3000/api/produtos/createProduct", data, {
+      headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAZ21haWwuY29tIiwicGFzc3dvcmQiOiIkMmIkMTAkdEFWR1RQSi8vb0VSWks5YlVPWXhrZXo4UnUvaWlwLlRlZmJtOVhZWDZMam5TWHF2VDZ5ZksiLCJpYXQiOjE3MDc0MjMxMzUsImV4cCI6MTcwNzQyNjczNX0.zZxyPru6XtTp5lIk5udVLFh-huY3hvXMJB_a8qnLTOA`,
+      },
+    });
+    return "Produto cadastrado com sucesso!";
+  } catch (error) {
+    return { error: "Error ao cadastrar produto." + error};
+  }
+};
+
+export { GetProductCategories, CreateProduct };
